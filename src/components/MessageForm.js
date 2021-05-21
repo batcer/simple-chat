2e12d37d96ca5efad976ace7eb4374225b82a499
Mikhail Batcer <mbatser@yandex.ru>
2021-05-21 14:59:38 +0300

    Clear message form on channel switch

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,24 +1,37 @@
-import React, {createRef, useContext} from 'react';
+import React, {createRef, useContext, useEffect} from 'react';
 import {v4 as uuid} from 'uuid';
-
 import {StoreContext} from '../context';
+import {autorun} from 'mobx';
 import {observer} from 'mobx-react';
-import {Message} from '../store/Message';
 
 import style from './MessageForm.module.css';
 
 const MessageForm = () => {
-    const {currentChannel, getContactById, myself} = useContext(StoreContext);
+    const {currentChannel} = useContext(StoreContext);
+
+    useEffect(() => {
+        if (currentChannel) {
+            clearTextInput();
+        }
+    }, [currentChannel])
+
     if (!currentChannel) {
         return null;
     }
 
     const inputRef = createRef();
 
+    const clearTextInput = () => {
+        if (!inputRef.current) {
+            return;
+        }
+        inputRef.current.innerText = '';
+    }
+
     const postMessage = () => {
         const text = inputRef.current.innerText;
         currentChannel.addMessageWithText(text);
-        inputRef.current.innerText = '';
+        clearTextInput();
     }
 
     return (
